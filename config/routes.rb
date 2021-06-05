ede41d3c07949362b2383070cb84f0c086be120c
EC2 Default User <ec2-user@ip-172-31-55-72.ec2.internal>
2021-06-05 22:46:08 +0000

    [modify]test erros

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {   registrations: 'users/registrations',
                                     sessions: 'users/sessions' }
   root to: "homes#top"
-  get "/homes/about" => "homes#about"
+  get "/home/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  
-  
+
+
+
   resources :users, only: [:show, :edit, :update,:index]
   resources :books
 end
