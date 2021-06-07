6e72f5420f14e4c26bfd09a3c12557c584b6b33c
EC2 Default User <ec2-user@ip-172-31-55-72.ec2.internal>
2021-06-07 09:48:33 +0000

    [submit]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,12 @@ class ApplicationController < ActionController::Base
      user_path(current_user.id)
   end
 
+
+
   protected
 
   def configure_permitted_parameters
     # ログイン時に名前とパスワードが必要な為、sign_upだけでなく、loginの記述が必要かも？
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
-    # できればまとめたい！
-    devise_parameter_sanitizer.permit(:sign_in, keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :email])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-
   resources :users, only: [:show, :edit, :update,:index]
   resources :books
 end
